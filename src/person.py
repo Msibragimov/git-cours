967b543b6ba7d273202af584fd74cbc8c3aee682
Msibragimov <msibragimov1@gmail.com>
2021-08-27 18:11:15 +0500

    Problem was defused

--- src/person.py
+++ src/person.py
@@ -69,9 +69,7 @@ class Customer(Person):
             raise Exception('No remaining visits')
         self._remaining_visits -= 1
         self._total_visits += 1
-        if self._total_visits%5 == 0:
-            self._remaining_visits += 1
-
+        
     @property
     def is_active(self):
         return self._is_active
@@ -81,4 +79,5 @@ class Customer(Person):
 
     def unlock(self):
         self._is_active = True
-        
+        if self._total_visits%5 == 0:
+            self._remaining_visits += 1
